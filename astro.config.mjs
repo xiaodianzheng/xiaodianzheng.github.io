cab3f7f791d6933df47cb38271070c7ad931cff6
xiaodianzheng <xdian.zheng@gmail.com>
2023-02-15 21:08:52 +0800

    update config and index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xiaodianzheng.github.io',
-  base: '/personal-website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
