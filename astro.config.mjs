b4db55ec63d8f1f7854a2c7ba80b24b179c718f0
xiaodianzheng <xdian.zheng@gmail.com>
2023-02-15 20:30:29 +0800

    deploy on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://xiaodianzheng.github.io',
+  base: '/personal-website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
